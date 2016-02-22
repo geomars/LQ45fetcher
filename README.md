e07c0677a490f4ae4b5a5e179efcd1d3da4def1c
geomars <egogeomars@gmail.com>
2016-02-22 14:45:10 +0700

    add url for live

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LQ45fetcher
 
 Jakarta Stock Exchange index LQ45 visualizer
+
+Running on 158.85.190.238:5000
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # project/run.py
-
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='158.85.190.238', port=port)
